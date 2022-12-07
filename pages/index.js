921b480df83eb387b2326f638ae18b57cdcc4180
unknown <lknsaheed@gmail.com>
2022-12-07 22:26:42 +0100

    Added google site verification

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,16 @@ export default function Main() {
   return (
     <div>
       <Head>
+        <title>Chaindustry | Home</title>
+        <meta
+          name="description"
+          content="Chaindustry is a digital servicing company giving value and digital services to its users and partners."
+        />
+        <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Chaindustry is a DoToEarn network offering value and digital services to it's users and partners."
         />
-
         <meta property="og:url" content="https://www.chaindustry.io" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Chaindustry" />
@@ -22,7 +27,6 @@ export default function Main() {
           property="og:image"
           content="https://firebasestorage.googleapis.com/v0/b/cashio-1ccdd.appspot.com/o/og_chain-min%20(1).png?alt=media&token=aa672469-787f-41f3-84e1-c0956951e51e"
         />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="chaindustry.io" />
         <meta property="twitter:url" content="https://www.chaindustry.io" />
@@ -34,14 +38,11 @@ export default function Main() {
         <meta
           name="twitter:image"
           content="https://firebasestorage.googleapis.com/v0/b/cashio-1ccdd.appspot.com/o/og_chain-min%20(1).png?alt=media&token=aa672469-787f-41f3-84e1-c0956951e51e"
-        />
-
-        <title>Chaindustry | Home</title>
+        />{" "}
         <meta
-          name="description"
-          content="Chaindustry is a digital servicing company giving value and digital services to its users and partners."
+          name="google-site-verification"
+          content="kg2__dhEGnrztOnTcU5__JFGL2vzk8APpbt9CKiqQwM"
         />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Home />
     </div>
